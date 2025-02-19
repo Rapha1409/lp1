670ef0fe376edb98fbca9ff22f50b7cbab173cff
Rapha1409 <raphaelanchietagoncalvesfilho@gmail.com>
2025-02-19 20:34:35 -0300

    Add files via upload
    impar ou par

---
+++ main (1).cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream> 
+using namespace std; 
+int main()
+{
+    
+     
+    int num=0; 
+    cout<<"escolha um numero\n"; 
+    cin >> num; 
+    
+    if(num%2==0){  
+    cout << "par";} 
+    else{
+    cout << "impar";
+    }
+    
+    return 0;
+}
