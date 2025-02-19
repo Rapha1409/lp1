c17b567d92cb8a42a289bd0c8b00f0dd5809bd5f
Rapha1409 <raphaelanchietagoncalvesfilho@gmail.com>
2025-02-19 20:21:50 -0300

    Add files via upload
    exercício maior q 20

---
+++ principal.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+int num =0; 
+    cout <<"esclha um numero"; 
+    cin >>num ; 
+    
+    if(num>20){ 
+    cout<<"o valor eh"<<(num/2); 
+    }
+    else{ 
+    cout<<"o valor eh"<<num;  
+    } 
+    return 0; 
+    } 
+    
